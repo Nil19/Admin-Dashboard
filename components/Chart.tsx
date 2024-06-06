23f3c55af8a77e2d316efe1795d58481ce614939
Nil19 <nilufar_adat@hotmail.com>
2024-06-06 17:04:57 +0100

    chart updates

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -4,41 +4,41 @@ import { Card, AreaChart, Title, Text } from "@tremor/react";
 
 const data = [
   {
-    Month: "Jan 24",
-    Sales: 2890,
-    Profit: 2400,
+    Month: "Jan",
+    2024: 2890,
+    2023: 1200,
   },
   {
-    Month: "Feb 24",
-    Sales: 1890,
-    Profit: 1398,
+    Month: "Feb",
+    2024: 1890,
+    2023: 3343,
   },
   {
-    Month: "Mar 24",
-    Sales: 3890,
-    Profit: 3430,
+    Month: "Mar",
+    2024: 3453,
+    2023: 2657,
   },
   {
-    Month: "Apr 24",
-    Sales: 5250,
-    Profit: 4790,
+    Month: "Apr",
+    2024: 4250,
+    2023: 3550,
   },
   {
-    Month: "May 24",
-    Sales: 3970,
-    Profit: 3510,
+    Month: "May",
+    2024: 6970,
+    2023: 3510,
   },
 ];
 
 export default function Chart() {
   return (
     <Card className="mt-8">
-      <Title>Performance</Title>
-      <Text>Comparison between Sales and Profit</Text>
+      <Title>Sales</Title>
+      <Text>Comparison between 2023 and 2024 sales</Text>
       <AreaChart
         className="mt-4 h-80"
         data={data}
-        categories={["Sales", "Profit"]}
+        categories={["2024", "2023"]}
         index="Month"
         colors={["emerald", "sky"]}
         valueFormatter={(number: number) =>
